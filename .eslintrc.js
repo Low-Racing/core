dce922c27393cdcf481f8bfb9692525572e0c30c
Julio Developer <juliodeveloper777@gmail.com>
2025-07-19 13:35:50 -0300

    feat...

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',      // libera o uso de any
-    '@typescript-eslint/no-unused-vars': 'warn',     // variáveis não usadas viram warnings
-    'prefer-const': 'warn',                          // let onde pode const vira warning
-    'react/no-unescaped-entities': 'off'             // para evitar erros com aspas no JSX
-  }
-}
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthOptions } from "next-auth";
