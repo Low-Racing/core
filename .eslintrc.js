c3f2e477fcd039248eb206fac978836b4a2a4630
Julio Developer <89735413+JulioDeveloper777@users.noreply.github.com>
2025-07-19 13:24:16 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   extends: ['next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-    'prefer-const': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',      // libera o uso de any
+    '@typescript-eslint/no-unused-vars': 'warn',     // variáveis não usadas viram warnings
+    'prefer-const': 'warn',                          // let onde pode const vira warning
+    'react/no-unescaped-entities': 'off'             // para evitar erros com aspas no JSX
   }
 }
