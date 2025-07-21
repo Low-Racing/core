99eeab467a0e16409e466959f327f4c3cb4b0f6e
Julio Developer <juliodeveloper777@gmail.com>
2025-07-20 22:01:34 -0300

    ....

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+};
+
+module.exports = nextConfig;
